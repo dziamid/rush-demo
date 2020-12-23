191843713a5a30f2331b14beba0f462ebc2f695f
Dziamid Zayankouski <dziamid@gmail.com>
2020-12-23 20:52:17 +0300

    chore: imported core project

---
+++ core/src/index.ts
@@ -0,0 +1,9 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+  // import "core-js/fn/array.find"
+  // ...
+export default class DummyClass {
+
+}
+
+export const foo = () => 125
+export const baz = () => 124
