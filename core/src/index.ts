fa7af041ba3b4ed4e3789e5585d463b938b30638
Dziamid Zayankouski <dziamid@gmail.com>
2020-12-25 18:43:39 +0300

    updated core

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,9 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export class DummyClass {
 
-}
-
-export const baz = () => 2240
-export const foo = () => 2251
+export const baz = () => 2241
+export const foo = () => 2252
